71ed74521cfb2f695818492306236534cee0f798
Parth's MacBook Air <parthjha@Parths-MacBook-Air.local>
2026-06-07 17:44:26 +0530

    Debug Xcode Cloud PATH

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,17 +2,10 @@
 
 set -e
 
-export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
+echo "PATH=$PATH"
 
-echo "Node version:"
 which node || true
-node --version || true
-
-echo "NPM version:"
 which npm || true
-npm --version || true
 
-echo "Installing CocoaPods dependencies..."
-pod install
+echo "Skipping pod install in post-clone"
 
-echo "Finished setup."
